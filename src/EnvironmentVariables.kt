b83636d5115331f2d4c209767368a98ca45f379d
Rémy LAVERGNE-PRUDENCE <lavergne.remy@gmail.com>
2020-01-07 23:18:34 +0100

    Export some constant name + get environment variables at startup

--- src/EnvironmentVariables.kt
+++ src/EnvironmentVariables.kt
@@ -7,8 +7,15 @@ object EnvironmentVariables {
     lateinit var emailObject: String
     lateinit var emailBody: String
 
+    lateinit var smtpServer: String
+    var smtpPort: String = DEFAULT_SMTP_PORT
+    lateinit var smtpUser: String
+    lateinit var smtpPassword: String
+
+
     @Throws(MissingEnvironmentVariables::class)
     fun getEnvironmentVariables() {
+        // TODO: Delete this and set via Frontend or API
         this.senderEmail = System.getenv(SENDER_EMAIL)
             ?: throw MissingEnvironmentVariables("Environment variables are missing : SENDER_EMAIL")
         this.receiverEmail = System.getenv(RECEIVER_EMAIL)
@@ -17,5 +24,15 @@ object EnvironmentVariables {
             ?: throw MissingEnvironmentVariables("Environment variables are missing : EMAIL_OBJECT")
         this.emailBody = System.getenv(EMAIL_BODY)
             ?: throw MissingEnvironmentVariables("Environment variables are missing : EMAIL_BODY")
+
+        // SMTP configuration (most important)
+        this.smtpServer = System.getenv(SMTP_SERVER)
+            ?: throw MissingEnvironmentVariables("Environment variables are missing : SMTP_SERVER")
+        this.smtpPort = System.getenv(SMTP_PORT)
+            ?: throw MissingEnvironmentVariables("Environment variables are missing : SMTP_PORT")
+        this.smtpUser = System.getenv(SMTP_USER)
+            ?: throw MissingEnvironmentVariables("Environment variables are missing : SMTP_USER")
+        this.smtpPassword = System.getenv(SMTP_PASSWORD)
+            ?: throw MissingEnvironmentVariables("Environment variables are missing : SMTP_PASSWORD")
     }
 }
