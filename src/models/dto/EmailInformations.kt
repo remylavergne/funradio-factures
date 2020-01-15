d496f2443e52a228c1b8883b5e2b1234c18bb792
Rémy Lavergne <lavergne.remy@gmail.com>
2020-01-15 12:59:43 +0100

    more informations when a new email is created

--- src/models/dto/EmailInformations.kt
+++ src/models/dto/EmailInformations.kt
@@ -3,7 +3,9 @@ package dev.remylavergne.models.dto
 data class EmailInformations(
     var receiverEmail: String = "",
     var mailTitle: String = "",
-    var mailBody: String = ""
+    var mailBody: String = "",
+    var repeat: Long = 0,
+    var delayMillis: Long = 10000
 ) {
     fun areValid() = receiverEmail.isNotEmpty() && mailTitle.isNotEmpty() && mailBody.isNotEmpty()
 }
